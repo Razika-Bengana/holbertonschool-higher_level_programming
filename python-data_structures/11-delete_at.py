4c4f33f805e4798648ddf3f83411a61b4156d341
Razika-Bengana <5003@holbertonschool.com>
2022-09-14 23:53:38 +0200

    Python

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,5 +4,4 @@ def delete_at(my_list=[], idx=0):
                 del my_list[idx]
                 return (my_list)
         else:
-                if idx <= 0 and idx >= len(my_list):
-                        return (my_list)
+                return (my_list)
