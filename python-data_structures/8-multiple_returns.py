16e6d4b5eaa87eac9700abcbddcf3840231bbb8c
Razika-Bengana <5003@holbertonschool.com>
2022-09-14 06:33:55 +0200

    Python

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,5 @@ def multiple_returns(sentence):
     i = sentence[0]
     if len(sentence) == 0:
         i = None
-        return (0, None)
     else:
-        return (len(sentence), sentence[0])
+        return (len(sentence), i)
