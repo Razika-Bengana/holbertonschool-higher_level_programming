a52e5bfe58d22e2563e40620f6fc1f1aa38f67cf
Razika-Bengana <5003@holbertonschool.com>
2022-09-18 00:04:36 +0200

    Python

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in range(len(new_list)):
+    for i in range(0, len(new_list)):
         new_list = [replace if new_list[i] == search else new_list[i]
                     for new_list[i] in new_list]
         return (new_list)
