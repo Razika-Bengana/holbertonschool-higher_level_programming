4f6bae34ab6909b44ae52c9fbef3a75f1a8790a0
Razika-Bengana <5003@holbertonschool.com>
2022-09-18 01:59:02 +0200

    Python

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        new_dict = a_dictionary[key] * 2
-        return (new_dict)
+    return {key: value*2 for key, value in a_dictionary.items()}
