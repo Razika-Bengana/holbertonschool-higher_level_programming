a03b1c09089ca64914078f09070dc2145aa866c8
Razika-Bengana <5003@holbertonschool.com>
2022-09-18 02:03:40 +0200

    Python

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(0, len(new_list)):
-        new_list = [replace if new_list[i] == search else new_list[i]
-                    for new_list[i] in new_list]
-        return (new_list)
+    new_list = [replace if i == search else i for i in my_list]
+    return new_list
