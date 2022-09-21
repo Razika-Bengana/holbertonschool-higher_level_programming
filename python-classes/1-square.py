1e692b47e249595033fbd020dfc8cbe297afe2b9
Razika-Bengana <5003@holbertonschool.com>
2022-09-21 14:41:45 +0200

    Python

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class for Square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """define the private instance attribute size"""
+        self.__size = size
