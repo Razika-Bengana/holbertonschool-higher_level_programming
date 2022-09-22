220d2b28dc12bd0cd101f52366dd1207f65ecd11
Razika-Bengana <5003@holbertonschool.com>
2022-09-22 14:19:38 +0200

    Python

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define a class for Square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """initialization of a new square"""
+        self.__size = size
+        """define the private instance attribute size"""
+
+    @property
+    def size(self):
+        """public instance method that get the value of the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return (self.__size)**2
+
+    def my_print(self):
+        """Public instance method that prints in stdout the square with #"""
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
+            if (self.__size) == 0:
+                print()
