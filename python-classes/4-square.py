81efc97fab910a07935c98804a51ddcfd2d49476
Razika-Bengana <5003@holbertonschool.com>
2022-09-22 15:40:10 +0200

    Python

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,17 +5,18 @@
 class Square:
     """Square class"""
     def __init__(self, size=0):
-        """initialization of a new square"""
+        """class instatiation with option"""
         self.__size = size
         """define the private instance attribute size"""
 
     @property
     def size(self):
-        """public instance method that get the value of the size"""
+        """proprety setter to retrieve the size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """proprety setter to set the size"""
         self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
