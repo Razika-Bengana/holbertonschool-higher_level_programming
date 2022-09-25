74f77ab6f88fe47f0756602c9b75bac4274fd85c
Razika-Bengana <5003@holbertonschool.com>
2022-09-25 16:41:23 +0200

    Python

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle():
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
