0634c2ad30c5d9b9f448e0b98e9c394e2ba5fa03
Razika-Bengana <5003@holbertonschool.com>
2022-09-25 23:14:08 +0200

    Python

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -53,5 +53,10 @@ class TestMaxInteger(unittest.TestCase):
         result = max_integer(l)
         self.assertEqual(result, 5)
 
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value: it should return this max"""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
     if __name__ == '__main__':
         unittest.main()
