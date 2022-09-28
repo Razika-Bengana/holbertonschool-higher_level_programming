3684d15e4f5e7c05f90ea636ed451c7f843068e1
Razika-Bengana <5003@holbertonschool.com>
2022-09-28 11:57:24 +0200

    Python

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+return list of object's attributes and methods
+"""
+
+
+def lookup(obj):
+    """function that returns the list of available attributes
+       and methods of an object"""
+    return dir(obj)
