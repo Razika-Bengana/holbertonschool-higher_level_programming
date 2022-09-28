aecb04b097f9fa0662404436d2bf734c4c6440a9
Razika-Bengana <5003@holbertonschool.com>
2022-09-28 23:58:34 +0200

    Python

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""define an empty class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class defined that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """
+            instantiation with size that must be private,
+            and must be a positive integer
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Public instance method that return the area"""
+        return self.__size ** 2
