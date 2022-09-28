2a2fa13518b2214e9f8f37826fc3184935776c95
Razika-Bengana <5003@holbertonschool.com>
2022-09-28 12:10:27 +0200

    Python

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""define a class that inherits from a list"""
+
+
+class MyList(list):
+    """make a class 'MyList' that inherits from 'list'"""
+    pass
+
+
+def print_sorted(self):
+    """public instance method that prints the list in sorted order"""
+    print(sorted(self))
