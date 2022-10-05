075097d89e5e512ac0ef6503c9ce664749787b78
Razika-Bengana <5003@holbertonschool.com>
2022-10-05 10:18:25 +0200

    Python

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -15,3 +15,15 @@ class Square(Rectangle):
         """ the overloading method __str__ """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
                                                          self.y, self.width)
+
+    @property
+    def size(self):
+        """ private instance attribute of size """
+        """ get the value of size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ set the value of size """
+        self.width = value
+        self.height = value
