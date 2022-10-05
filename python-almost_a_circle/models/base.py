9091ef2f77227348e35d224bb1823ad49ed0b853
Razika-Bengana <5003@holbertonschool.com>
2022-10-05 11:02:41 +0200

    Python

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,7 +3,7 @@
 define a new class that will represent the base
 of the other classes in this project
 """
-
+import json
 
 class Base:
     """ define this class """
@@ -18,3 +18,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+            static method that returns the JSON string
+            representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
