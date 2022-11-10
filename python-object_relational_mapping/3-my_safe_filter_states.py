d4824ff177193e8c51a0aeb288cf203825cdd378
Razika-Bengana <5003@holbertonstudents.com>
2022-11-10 15:13:27 +0100

    safe script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                    ORDER BY states.id ASC", (argv[4],))
+    rows = cursor.fetchall()
+    for state in rows:
+        print(state)
