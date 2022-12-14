30ed7037bc79d81ab91115ada4c4faf13ed41365
Razika-Bengana <5003@holbertonstudents.com>
2022-12-14 20:17:58 +0100

    9. Say Hello!

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+const $ = window.jQuery;
+$(document).ready(function () {
+  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
